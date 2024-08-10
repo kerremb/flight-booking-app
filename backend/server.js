018bbc0eeae68b1e57c7dcf8999f73e5efafc1d1
Kerem <bayraktarrkerem@gmail.com>
2024-08-11 01:37:41 +0300

    backend-created

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT | 5000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Success");
+});
+
+app.listen(PORT, () => console.log(`Port Listenin at ${PORT}`));
